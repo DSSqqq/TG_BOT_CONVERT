3e78701133c5e511d4b139ffac223dfde6572c2a
DSSqqq <danilka780@gmail.com>
2024-05-09 00:00:33 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN ='7169446989:AAGkJJ2am81Sca4qqSXv1EgF7wuYHiXIgsc'
+
+keys= {
+    'доллар': 'USDT',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'тон': 'TON'
+}
